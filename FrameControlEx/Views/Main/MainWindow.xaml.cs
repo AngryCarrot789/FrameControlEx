94107f96b63a9465dd2a43405db283689aea902e
REghZy <kettlesimzulator420@gmail.com>
2023-05-18 13:02:13 +0100

    Made main UI

---
+++ FrameControlEx/Views/Main/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using FrameControlEx.Views;
+
+namespace FrameControlEx {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : WindowEx {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
