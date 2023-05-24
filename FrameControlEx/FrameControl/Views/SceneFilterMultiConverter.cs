b7922188e701a91c3d8a443166fec87143b4deb9
REghZy <kettlesimzulator420@gmail.com>
2023-05-24 01:39:30 +0100

    Stuff

---
+++ FrameControlEx/FrameControl/Views/SceneFilterMultiConverter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using FrameControlEx.Core.FrameControl.Scene;
+using FrameControlEx.Core.Utils;
+
+namespace FrameControlEx.FrameControl.Views {
+    public class OutputFilterMultiConverter : IMultiValueConverter {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values.Length != 2) {
+                throw new Exception($"Expected 2 values, not {values.Length}");
+            }
+
+            OutputViewModel vm = (OutputViewModel) values[0];
+            Predicate<OutputViewModel> func = (Predicate<OutputViewModel>) values[1];
+            return (func == null || func(vm)).Box();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
